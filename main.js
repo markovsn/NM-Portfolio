dbdcd88ff5815bc54738ec4fc47d51d7e44403fb
markovsn <100452705+markovsn@users.noreply.github.com>
2022-09-07 12:00:21 +1000

    updated links

--- main.js
+++ main.js
@@ -41,3 +41,10 @@ jQuery(function($) {
     }
 
     
+    $("nav").find("a").click(function(e) {
+      e.preventDefault();
+      var section = $(this).attr("href");
+      $("html, body").animate({
+          scrollTop: $(section).offset().top
+      });
+  });
